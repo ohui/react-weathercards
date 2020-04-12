4e4012428562ae5cc4c651d8e114760ed2f8037c
ohui <hui3@uic.edu>
2020-04-12 11:55:23 -0700

    Updates

---
+++ precache-manifest.1e76d5dd9f0a52bf9f679c356766c51e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd9388b6206e2036bbeb772c1160777",
+    "url": "/react-weathercards/index.html"
+  },
+  {
+    "revision": "d6c1442eb4428d31221a",
+    "url": "/react-weathercards/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3ed7135ab627590db7cc",
+    "url": "/react-weathercards/static/js/2.f3bc47f8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-weathercards/static/js/2.f3bc47f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6c1442eb4428d31221a",
+    "url": "/react-weathercards/static/js/main.2dd05e08.chunk.js"
+  },
+  {
+    "revision": "3c0441ffcaf56c79ba5b",
+    "url": "/react-weathercards/static/js/runtime-main.8d77e648.js"
+  }
+]);
